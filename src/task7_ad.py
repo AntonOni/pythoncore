f51551e50483051c3ee04d84b1b07186ccc64958
AntonOni <44893267+AntonOni@users.noreply.github.com>
2018-12-01 07:16:14 -0800

    changes

--- src/task7_ad.py
+++ src/task7_ad.py
@@ -1,6 +1,14 @@
 import json
+import os
 
 dollars10_18 = [30.16, 29.32, 29.89, 30.30, 36.04, 70.22, 83.59, 58.29, 62.41]
+
+if os.path.exists('result1.txt'):
+    os.remove('result1.txt')
+
+if os.path.exists('result2.txt'):
+    os.remove('result2.txt')
+
 
 
 def rasschet_stoimosti_with_dict(dollars10_18, stoimost_pomesheniya=20000, stoimost_arendy_kvadrata=50, kvadraty=95):
@@ -33,19 +41,27 @@ def rasschet_stoimosti_with_dict(dollars10_18, stoimost_pomesheniya=20000, stoim
         result[god]=template
         #print('В {} году стоимость помещения была равна {} рублей. Стоимость аренды равнялась {} рублей. Обслуживание его обошлось в {} рублей. В итоге профит от подорожания жилья и сдачи его стоставил {} рублей.'.format(god, dinanica_cen, dinamica_arendy, obsluga, profit))
         dict1 = {'god' : god, 'dinanica_cen' : dinanica_cen, 'dinamica_arendy' : dinamica_arendy, 'obsluga' : obsluga, 'profit' : profit}
-        with open('result1.txt', 'w') as fp:
+        with open('result2.txt', 'a') as fp:
+            print("  [+] append data from function rasschet_stoimosti_with_dict to file result2.txt")
             json.dump(dict1, fp)
+            fp.write("\n")
 
         god = god + 1
     return result
 
-
-with open('result.txt', 'r') as fp:
-    print(json.load(fp))
-
+print("[!] Call function: rasschet_stoimosti_with_dict...")
 funct = rasschet_stoimosti_with_dict(dollars10_18)
-print(funct)
+# print(funct)
+print("[+] Complete...")
 
+print("[+] Read data from file result2.txt")
+with open('result2.txt', 'r') as fp:
+    fromfile = fp.read()
+    l = fromfile.split("\n")
+    for _ in l:
+        if _ != "":
+            print("  - {}".format(json.loads(_)))
+print("[+] Complete...")
 
 def rasschet_stoimosti_with_list(dollars10_18, stoimost_pomesheniya=20000, stoimost_arendy_kvadrata=50, kvadraty=95):
     """
@@ -84,25 +100,30 @@ def rasschet_stoimosti_with_list(dollars10_18, stoimost_pomesheniya=20000, stoim
         )
         # Сформерованный словарь с результатами template я добавляю как элемент списка результата result
         result.append(template)
-        with open('result1.txt', 'w') as fp:
-            json.dump(result, fp)
+
+        # with open('result1.txt', 'w') as fp:
+        #     json.dump(result, fp)
 
         god = god + 1
+    result = dict(result=result)
     return result
 
 
-with open('result.txt', 'r') as fp:
-    print(json.load(fp))
+
+
+print('Вызывыем функцию rasschet_stoimosti_with_list')
 
 
 funct = rasschet_stoimosti_with_list(dollars10_18)
 print(funct)
 
+with open('result1.txt', 'w') as fp:
+    json.dump(funct, fp)
 
-
-
-
-
+print("Закончили")
+print('Читаем файл')
+with open('result1.txt', 'r') as fp:
+     print(json.load(fp))
 # with всегда хочет чтобы файс с которым он должен работать имел название
 
 
